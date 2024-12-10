8ecb55c609313b5a499958c81e96b28a07536582
Bogdan3000 <ihnatenko.bogdan@gmail.com>
2024-12-11 00:07:59 +0100

    added style.css + static folder

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 
 # Подключаем статику для доступа к загруженным файлам
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
-
+app.mount("/static", StaticFiles(directory="static"), name="static")
 # Подключаем маршруты из routes.py
 app.include_router(router)
